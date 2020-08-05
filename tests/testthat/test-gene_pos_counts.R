56f7de2b08d2915787e5122e35db814a4b3d7308
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-08-05 18:16:47 -0400

    test case of SNP with 0 sum. SNP unavailable

--- tests/testthat/test-gene_pos_counts.R
+++ tests/testthat/test-gene_pos_counts.R
@@ -1,8 +1,19 @@
 context("check gene_pos_counts function")
 
+test_noSNP<-data.frame("BP"=c(12111),"SNP"=c("SNP11")) ## no SNP in the raw data - throw an error as the bounadries are not there for the SNP
+
+test_noSNPinraw<-data.frame("BP"=c(1500),"SNP"=c("SNP11")) ## no SNP in the raw data but present in the boundaries
+
+test_SNP_nosum<-data.frame("BP"=c(27000),"SNP"=c("SNP10"))  ##return null as no person carries allele of this SNP
+
+##test with raw data that has SNP with 0 allele count
+##test with raw data that has SNP but not in the boundaries
+
 test_that("function testing",{
               expect_error(gene_pos_counts(-4, 1,1))
 	expect_is(gene_pos_counts(recodedgen,  snppos, genecoord),'data.table')
+	expect_error(gene_pos_counts(recodedgen, test_noSNP,genecoord))
+expect_null(gene_pos_counts(recodedgen, test_SNP_nosum,genecoord))
 })
 
 
