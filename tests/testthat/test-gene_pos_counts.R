ffa619e57a7fbe52eb616700a6e00cadac9c8d76
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-08-05 16:52:25 -0400

    gene_pos test case

--- tests/testthat/test-gene_pos_counts.R
+++ tests/testthat/test-gene_pos_counts.R
@@ -1,2 +1,8 @@
 context("check gene_pos_counts function")
 
+test_that("function testing",{
+              expect_error(gene_pos_counts(-4, 1,1))
+	expect_is(gene_pos_counts(recodedgen,  snppos, genecoord),'data.table')
+})
+
+
