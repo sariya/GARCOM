e6df4ea9eb4e2b4c51f71b875c9c2a4b0a0f17ee
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-08-05 16:47:27 -0400

    snp-gene annot test case

--- tests/testthat/test-gene_annot_counts.R
+++ tests/testthat/test-gene_annot_counts.R
@@ -1,3 +1,6 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("check gene_annot_counts function")
+
+test_that("function testing",{
+              expect_error(gene_annot_counts(-4, 1))
+	expect_is(gene_annot_counts(recodedgen, snpgene),'data.table')
 })
