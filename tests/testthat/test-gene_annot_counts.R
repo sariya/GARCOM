6aa25c2b1251ceccaeb093f2b645cc536306149d
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-08-06 14:14:17 -0400

    tests after running devtools cran check

--- tests/testthat/test-gene_annot_counts.R
+++ tests/testthat/test-gene_annot_counts.R
@@ -1,8 +1,8 @@
 context("check gene_annot_counts function")
 
-testexp_snpgene<-data.table("GENE"=c("GENE1"),"SNP"=c("SNP10")) ## 0 values in each person for this SNP
-testexp_snpgene_noSNP<-data.table("GENE"=c("GENE1"),"SNP"=c("SNP11")) ## no SNP in the raw data
-testexp_snpgene_pass<-data.table("GENE"=c("GENE1"),"SNP"=c("SNP4")) ## we'll return on row
+testexp_snpgene<-data.table::data.table("GENE"=c("GENE1"),"SNP"=c("SNP10")) ## 0 values in each person for this SNP
+testexp_snpgene_noSNP<-data.table::data.table("GENE"=c("GENE1"),"SNP"=c("SNP11")) ## no SNP in the raw data
+testexp_snpgene_pass<-data.table::data.table("GENE"=c("GENE1"),"SNP"=c("SNP4")) ## we'll return on row
 
 test_gen_withnoSNP<-data.frame("FID"=c(paste("FID",seq(1:10),sep="")),"IID"=c(paste("IID_sample",seq=(1:10),sep="")),
 "PAT"=c(rep(0,10)),"MAT"=c(rep(0,10)),"SEX"=c( c( c(rep(1,5)),rep(0,5))),"PHENOTYPE"=c( c(rep("NA",2),rep("1",4),
