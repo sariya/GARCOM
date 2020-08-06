dcb1e81f8f8f002337e185a5b3b91ae91fa43f9f
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-08-06 13:42:25 -0400

    documentation and code for data shipped

---
+++ R/data.R
@@ -0,0 +1,42 @@
+#' data set prepared for the package
+
+#' recodedgen is genetic data is 10 rows and 16 columns.
+#' Where the first 6 columns are FID, IID, PAT, MAT, SEX and PHENOTYPE which are inherent from the 
+#' PLINK output. Columns followed by PHENOTYPE are SNP names followed by _A, or _C
+#' or _T or _G that is allele. Each SNP column may have 0, 1, 2 or NA value
+
+recodedgen<-data.frame("FID"=c(paste("FID",seq(1:10),sep="")),
+"IID"=c(paste("IID_sample",seq=(1:10),sep="")),
+"PAT"=c(rep(0,10)),"MAT"=c(rep(0,10)),
+"SEX"=c( c( c(rep(1,5)),rep(0,5))),
+"PHENOTYPE"=c( c(rep("NA",2),rep("1",4),
+rep("0",4))),"SNP1_A"=c(rep(1,1),rep(0,9)),
+"SNP2_T"=c(rep(1,2),rep(0,7),rep(NA,1)),
+"SNP3_G"=c(rep(0,2),rep(1,8)),
+"SNP4_C"=c(rep(NA,2),rep(0,7),rep(1,1)),
+"SNP5_C"=c(rep(NA,1),rep(0,8),rep(1,1)),
+"SNP6_G"=c(rep(1,10)),"SNP7_G"=c(rep(0,10)),
+"SNP8_C"=c(rep(0,10)), "SNP9_T"=c(rep(0,10)), "SNP10_A"=c(rep(0,10)))
+
+
+#' snpgene is SNP-gene annotation data with 10 rows and two columns. 
+#' Column names are GENE and SNP, where GENE column contains GENE names and SNP column
+#' contains SNP name that is annotated with the GENE
+snpgene<-data.frame("GENE"=
+c("GENE1","GENE2","GENE3","GENE4",
+"GENE5","GENE1","GENE1","GENE3","GENE2","GENE4"),
+"SNP"=c("SNP1","SNP2","SNP3",
+"SNP4","SNP5","SNP6","SNP7","SNP8","SNP9","SNP1"))
+
+#' snppos SNP-BP data with 2 columns and 10 rows. Column names are SNP and BP. 
+#' where SNP columns contains SNP names BP column contains position of the SNP
+
+snppos<-data.frame("SNP"=c(paste("SNP",seq(1:10),sep="")), "BP"=c(1100, 89200, 2500, 33000, 5500, 69500, 12000,8800, 23200, 27000))
+
+#' genecoord is Gene coordinate data with 3 columns and 5 rows. Column names are GENE, START and END
+#' where GENE column contains GENE name, START and END have information 
+#' for gene start and end coordinates respectively.
+
+genecoord<-data.frame("GENE"=c("GENE1","GENE2","GENE3","GENE4","GENE5"), 
+"START"=c(1000,2100,5000,40000,23000), 
+"END"=c(2000,3000,9000,45000,30000))
