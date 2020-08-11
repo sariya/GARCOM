be8a67c171f087a60e1b484e046aac50b6f86280
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-08-11 10:58:43 -0400

    adding comments and new function

--- R/utils.R
+++ R/utils.R
@@ -3,7 +3,16 @@
 garcom_check_column_names<-function(temp_data,col_names){
 ## 07 21 2020
 
+##two params: dataframe and vector of columns we are looking for.
+##return TRUE or FALSE
 return(col_names %in% colnames(temp_data))
 
 }
 ##function ends
+
+garcom_check_duplicates<function(temp_dataframe, column_name){
+
+##08/11/2020
+##
+print("")
+}
