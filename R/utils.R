b097de74ee8e4f31b764384ead78b6806b75f088
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-08-11 11:41:48 -0400

    function with duplicate data and column

--- R/utils.R
+++ R/utils.R
@@ -5,16 +5,23 @@ garcom_check_column_names<-function(temp_data,col_names){
 
 ##two params: dataframe and vector of columns we are looking for.
 ##return TRUE or FALSE
-    return(col_names %in% colnames(temp_data))
+return(col_names %in% colnames(temp_data))
 
 }
 ##function ends
 
-garcom_check_duplicates<function(temp_dataframe, column_name){
+garcom_check_duplicates<-function(temp_dataframe, column_name){
 
 ##08/11/2020
 ##two params: dataframe and second is column name for which we
 ##we'd like to ensure no duplicates are present
 ##
-    print("")
+if( length(unique(temp_dataframe[,get(column_name)])) !=length(temp_dataframe[,get(column_name)])){
+return(FALSE)
+} else{
+return(TRUE)
 }
+##check ends
+
+}
+##function ends
