e43672e7041ca9a31d7188385d4cdc09b34237ae
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-08-18 11:41:55 -0400

    added test cases

--- tests/testthat/test-gene_annot_counts.R
+++ tests/testthat/test-gene_annot_counts.R
@@ -23,4 +23,19 @@ test_that("function testing",{
     expect_null(gene_annot_counts(test_gen_withnoSNP , snpgene))
     expect_equal(nrow(gene_annot_counts(test_gen_with_oneSNP, snpgene)), 2)
 })
+
+test_that("function testing for subsetting",{
+
+expect_error(gene_annot_counts(recodedgen, snpgene,extract_SNP=c(""))) ##empty SNP names
+expect_error(gene_annot_counts(recodedgen, snpgene,extract_SNP=c("snp1"))) ##empty SNP names
+
+expect_error(gene_annot_counts(recodedgen, snpgene,keep_indiv=c(""))) ##empty iid
+expect_error(gene_annot_counts(recodedgen, snpgene,keep_indiv=c("snp1"))) ##wrong iid
+expect_error(gene_annot_counts(recodedgen, snpgene,filter_gene=c("snp1"))) ##wrong gene name
+
+    expect_is(gene_annot_counts(recodedgen,snpgene,filter_gene=c("GENE1")),'data.table')
+expect_is(gene_annot_counts(recodedgen, snpgene,keep_indiv=c("IID_sample8")),'data.table') ##just one sample id
+
+    expect_null(gene_annot_counts(recodedgen, snpgene,keep_indiv=c("IID_sample8"), filter_gene=c("GENE5"))) ## we provide IIDs, gene and subset. Return is null for this dataset
+})
 ##testing ends
