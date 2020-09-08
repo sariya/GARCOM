f430cc263f21df767ec9751ab7fa3cb62bcff717
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-09-08 11:36:55 -0400

    vcf with SNP and gene pos

---
+++ R/vcf_counts_withSNPpos.R
@@ -0,0 +1,12 @@
+
+
+
+##df_snpgene<- data.table(read.table("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/SOFTWARES/R_package_development/test_vcffiles/CHR22_snp.gene",header=TRUE))
+
+##pubvcf <- vcfR::read.vcfR("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/SOFTWARES/R_package_development/test_vcffiles/ALL.chr22.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz",verbose =TRUE)
+
+#dt_snp <-
+#dt_gene<-
+##vcf <- vcfR::read.vcfR("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/SOFTWARES/R_package_development/test_vcffiles/CHR22_ADC.vcf.gz", verbose=TRUE)
+
+
