ea8484425aa083367f92f95c54fbcf8a6259ed06
Sanjeev Sariya <sanjeevsariya@gmail.com>
2020-12-28 15:58:20 -0500

    minor edits to improve function

--- R/plink_count_snppos.R
+++ R/plink_count_snppos.R
@@ -1,23 +1,40 @@
-
+#'@title plink gene counts using SNP coords
+#'
+#'@description Function returns matrix with allelic counts per gene per individual for SNP and gene coordinates as inputs for PLINK (.bed) file format
+#'
+#'@details PLINK (.bed) file will be read
+#'
 plink_count_snppos<-function(plink_file,genecoord_dt,snp_pos_dt,snp_index,individuals_index){
-## added on 12 23 2020
 
-.libPaths(c("/home/ss5505/libraries_R/R_LIB4.0",.libPaths()))
-library(bigsnpr)
-library(data.table)
+#' @export
+#'
+#' @import data.table
+#'
+#' @importFrom bigsnpr snp_attach
+#' 
+#' @importFrom bigsnpr snp_readBed2
+
+#' @param plink_file plink .bed file with path 
+#'
+#' @param genecoord_dt a dataframe for gene boundaries with CHR START END GENE as column names. Where CHR should be integer 1-22. START and END column should be integer. GENE column contains gene names
+#'
+#' @param snp_pos_dt a dataframe for SNP information with SNP BP as column names.
+#'
+#' @param snp_index a vector of integer that specifies SNPs to read.
+#'
+#' @param individuals_index a vector of integer that specifies individuals to select. 
+#'
 
 plink_rds <- bigsnpr::snp_readBed2(plink_file,backingfile = tempfile(), ind.col=snp_index, ind.row=individuals_index)
 
      # Loading the data from backing files
-     data_plink <- snp_attach(plink_rds)
+     data_plink <- bigsnpr::snp_attach(plink_rds) ## added on 12 23 2020
 
 plink_snp_information_dt<- as.data.table(data_plink$map)
 plink_fam_information<-as.data.table(data_plink$fam)
 
 plink_genotype_dt<-  as.data.table(data_plink$genotypes[] )
 colnames(plink_genotype_dt) <-plink_snp_information_dt$marker.ID
-
-#     str(data_plink)     # dim(G <- data_plink$genotypes)    #  G[1:8, 1:8]
 cat("Genotypes have been loaded from plink file\n")
 
   snp_withingenes<- snp_pos_dt[genecoord_dt,c("SNP","GENE","START","END"),on=list(BP>=START,BP<=END),nomatch=0] # inner join ##https://stackoverflow.com/questions/63290994/foverlaps-data-table-error-ys-key-must-be-identical-to-the-columns-specified
@@ -56,17 +73,16 @@ plink_genotype_dt_subset<- plink_genotype_dt[,.SD,.SDcols=snps_intersect_index ]
 ## function ends
 
 
-snppos_dt<-data.table::fread("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/SOFTWARES/R_package_development/test_vcffiles/test_snppos.txt", header=TRUE)
-genecoordpass_dt<-data.table::fread("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/SOFTWARES/R_package_development/test_vcffiles/sample_genes.txt", header=TRUE)
-file_plink<-"/mnt/mfs/statgen/UKBiobank/data/exome_files/ukb23155_c22_b0_v1.bed"
+#snppos_dt<-data.table::fread("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/SOFTWARES/R_package_development/test_vcffiles/test_snppos.txt", header=TRUE)
+#genecoordpass_dt<-data.table::fread("/mnt/mfs/hgrcgrid/shared/GT_ADMIX/SOFTWARES/R_package_development/test_vcffiles/sample_genes.txt", header=TRUE)
+#file_plink<-"/mnt/mfs/statgen/UKBiobank/data/exome_files/ukb23155_c22_b0_v1.bed"
 
+#output_test<-plink_count_snppos(plink_file=file_plink,genecoord_dt=genecoordpass_dt,snp_pos=snppos_dt,snp_index=c(1:4000))
 
-output_test<-plink_count_snppos(plink_file=file_plink,genecoord_dt=genecoordpass_dt,snp_pos=snppos_dt,snp_index=c(1:4000))
+#output_testfiltered<-plink_count_snppos(plink_file=file_plink,genecoord_dt=genecoordpass_dt,snp_pos=snppos_dt,snp_index=c(1:4000),individuals_index=c(1:3000))
 
-output_testfiltered<-plink_count_snppos(plink_file=file_plink,genecoord_dt=genecoordpass_dt,snp_pos=snppos_dt,snp_index=c(1:4000),individuals_index=c(1:3000))
+#output_testfiltered<-plink_count_snppos(plink_file=file_plink,genecoord_dt=genecoordpass_dt,snp_pos=snppos_dt,snp_index=c(1:4000),individuals_index=c(1:3000))
 
-output_testfiltered<-plink_count_snppos(plink_file=file_plink,genecoord_dt=genecoordpass_dt,snp_pos=snppos_dt,snp_index=c(1:4000),individuals_index=c(1:3000))
-
-output_testfiltered_complete<-plink_count_snppos(plink_file=file_plink,genecoord_dt=genecoordpass_dt,snp_pos=snppos_dt,snp_index=c(1:414980),individuals_index=c(1:3000))
+#output_testfiltered_complete<-plink_count_snppos(plink_file=file_plink,genecoord_dt=genecoordpass_dt,snp_pos=snppos_dt,snp_index=c(1:414980),individuals_index=c(1:3000))
 
 
